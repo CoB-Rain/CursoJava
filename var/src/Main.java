994e8e7f908c6652f73e80bd22cb724bbe2764fc
Brian Costanzo <briancostanzo96@gmail.com>
2025-10-10 13:59:40 -0300

    funcionalidad var terminada

---
+++ var/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        /**
+         * Funcionalidad para declarar variables
+         * cuando utilizo la palabra reservada "var"
+         * quiero que java infiera en el tipo de variable de este.
+         */
+        var x = "Hola Mundo";
+        var y = 5;
+        var z = true;
+    }
+}
