29cc3abdb75c9e4b131a9335955842d4991d837d
Brian Costanzo <briancostanzo96@gmail.com>
2025-10-12 01:19:46 -0300

    Instruccion if.iml terminada

---
+++ Instruccion if/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main (String[] args){
+        int edad = 18;
+        //String movieClassification = "PG";
+        String movieClassification = "R18";
+        //String movieClassification = "RP18";
+        boolean isWithAdult = true;
+
+        boolean isRestricted = movieClassification.contains("R");
+
+        if (!isRestricted){
+            System.out.println("Puedes ver la película");
+        } else if (edad > 17) {
+            System.out.println("Puedes ver la película igual");
+        } else if (isWithAdult && movieClassification.contains("RP")) {
+            System.out.println("Viene con un adulto, puede ver la película");
+        } else {
+            System.out.println("No puedes ver la película");
+        }
+    }
+}
