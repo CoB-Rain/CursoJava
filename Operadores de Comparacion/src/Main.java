f29a5b5f0a1ed621560896bb05c35d7af0ec086e
Brian Costanzo <briancostanzo96@gmail.com>
2025-10-12 00:56:04 -0300

    Clase Operadores de Comparacion.iml terminada

---
+++ Operadores de Comparacion/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+        int a = 10, b = 10;
+
+        //comparacion por igualdad
+        System.out.println(a == b);
+        //comparacion por diferencia
+        System.out.println(a != b);
+        //comparacion si es mayor
+        System.out.println(a > b);
+        //comparacion si es mayor o igual
+        System.out.println(a >= b);
+        //comparacion si es menor
+        System.out.println(a < b);
+        //comparacion si es menor o igual
+        System.out.println(a <= b);
+    }
+}
