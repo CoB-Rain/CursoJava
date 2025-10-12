0b6a9d9542d0c495009a7afc82a5664777c1ad53
Brian Costanzo <briancostanzo96@gmail.com>
2025-10-12 01:33:08 -0300

    Clase Alcance.iml terminada

---
+++ Alcance/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args){
+        // para acceder a una variable debe estar
+        // en el mismo bloque de codigo de donde se quiere acceder.
+        // a esto se lo llama Alcance.
+
+        String mensaje = "Seco para programar";
+
+        /**
+         *         if(true){
+         *             String texto = "Hola Mundo";
+         *             System.out.println(texto);
+         *             if (true){
+         *                 System.out.println(texto);
+         *             }
+         *         }
+         *
+         *          la variable "texto" esta fuera del alcance de la aplicacion.
+         *          por eso no se puede mostrar en pantalla.
+         *          👇
+         *          System.out.println(texto); <-- ERROR!
+         */
+
+        if(true){
+            mensaje = "Hola Mundo";
+        }
+        System.out.println(mensaje);
+        System.out.println("fin de aplicación.");
+    }
+}
