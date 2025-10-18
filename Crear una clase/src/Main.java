41cd8ff5c419f04427c5677e8c9f765284b79aeb
Brian Costanzo <briancostanzo96@gmail.com>
2025-10-18 12:18:14 -0300

    Metodos a una clase aplicados. se realizaron cambios

--- Crear una clase/src/Main.java
+++ Crear una clase/src/Main.java
@@ -7,7 +7,15 @@ public class Main {
         User user2 = new User();
         user2.name = "Felipe";
         //imprimimos los dos user con sus propiedades.
+        System.out.println(user.name);
+        System.out.println(user2.name);
+
+        //imprimimos los id con sus valores iniciales
         System.out.println(user.id);
         System.out.println(user2.id);
+
+        //metodos saludar de cada User.
+        user.saludar();
+        user2.saludar();
     }
 }
--- Crear una clase/src/User.java
+++ Crear una clase/src/User.java
@@ -1,4 +1,10 @@
 class User {
     double id = Math.random(); //le damos un valor inicial a cada id (aleatorio)
     String name; //propiedad de la clase User
+
+    //metodo saludar de mi clase User.
+    //con la palabra reservada "this" hacemos referencia a la clase principal.
+    public void saludar() {
+        System.out.println("Hola mundo! soy " + this.name);
+    }
 }
