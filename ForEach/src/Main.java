ce13324ebacb31b1b8ec90f78755c541da4e38c3
Brian Costanzo <briancostanzo96@gmail.com>
2025-10-18 03:00:29 -0300

    Instrucción ForEach.iml terminada

---
+++ ForEach/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] personajes = { "Goku", "Vegeta", "Chanchito feliz"};
+
+//        for (int i = 0; i < personajes.length; i++) {
+//            System.out.println(personajes[i]);
+//        }
+
+        for (String personaje: personajes) {
+            System.out.println(personaje);
+        }
+    }
+}
