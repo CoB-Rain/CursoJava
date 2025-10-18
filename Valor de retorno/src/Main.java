400ffaf7ab96a4a9d5014e355d6950b691c0b27a
Brian Costanzo <briancostanzo96@gmail.com>
2025-10-18 10:58:12 -0300

    Valor de retorno.iml terminada

---
+++ Valor de retorno/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultado = suma(12, 25);
+        System.out.println(resultado);
+    }
+
+    //en este metodo, se retornara un valor del tipo int.
+    static int suma(int a, int b) {
+        return a + b;
+    }
+}
