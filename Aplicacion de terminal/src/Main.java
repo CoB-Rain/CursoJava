d096b9cc70427f277a1afda1ac154d8741c2aade
Brian Costanzo <briancostanzo96@gmail.com>
2025-10-18 02:36:08 -0300

    Aplicacion de terminal.iml terminada

---
+++ Aplicacion de terminal/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        while (!input.equals("salir")) {
+            System.out.println("Ingresa comando: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        }
+
+        System.out.println("Terminó el loop.");
+    }
+}
